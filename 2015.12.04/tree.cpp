7aa74903496c426a153688dbc29653a922d43ec1
kn2014 <cs1.8@mail.bg>
2015-12-04 14:54:11 +0200

    Update tree.cpp

--- 2015.12.04/tree.cpp
+++ 2015.12.04/tree.cpp
@@ -19,3 +19,11 @@ class Tree
 private:
 	TreeNode<T> *root;
 public:
+	Tree ()
+	Tree (const Tree<T>& t)
+	Tree<T>& operator = (const Tree<T> &t)
+	~Tree ()
+	bool empty ()
+	void setRoot (const T& newRoot)
+	void addSubTree (const Tree<T>& t)
+	void print (ostream &out)
